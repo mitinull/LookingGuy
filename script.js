a6dbfdc8a7707cd3420191b7779f32e91b56605b
mohammadT9030 <torabi9030@gmail.com>
2023-07-17 17:20:13 +0330

    2 eyes implemented

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-el = document.querySelector("div");
+[el1, el2] = document.querySelectorAll(".container");
 
-ox = 100;
-oy = 100;
+ox1 = 100;
+oy1 = 100;
+
+ox2 = 350;
+oy2 = 100;
 
 // console.log(el);
 
 addEventListener("mousemove", (object) => {
   // console.log(object.layerX, object.layerY);
-  el.style.rotate =
-    Math.PI * (object.layerX - ox < 0) +
-    Math.atan((object.layerY - oy) / (object.layerX - ox)) +
+  el1.style.rotate =
+    Math.PI * (object.layerX - ox1 < 0) +
+    Math.atan((object.layerY - oy1) / (object.layerX - ox1)) +
+    "rad";
+
+  el2.style.rotate =
+    Math.PI * (object.layerX - ox2 < 0) +
+    Math.atan((object.layerY - oy2) / (object.layerX - ox2)) +
     "rad";
 });
