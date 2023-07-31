b6670823e2e66d71fc60e6a18105955e92de68e0
mohammadT9030 <torabi9030@gmail.com>
2023-07-31 10:10:58 +0330

    hover effect and vertical character added

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-[el1, el2] = document.querySelectorAll(".container");
+[el1, el2] = document.querySelectorAll(".LOOKING_GUY__outer-eye");
 
-face = document.querySelector(".face-container");
+face = document.querySelector(".LOOKING_GUY");
 
 faceWidth = face.clientWidth;
 
-ox1 = el1.offsetLeft + el1.clientWidth / 2;
-oy1 = el1.offsetTop + el1.clientHeight / 2;
-console.log(ox1, oy1);
-
-ox2 = el2.offsetLeft + el2.clientWidth / 2;
-oy2 = el2.offsetTop + el2.clientHeight / 2;
-
-// console.log(el);
-
 addEventListener("mousemove", (object) => {
   // console.log(object);
+  ox1 = el1.offsetLeft + el1.clientWidth / 2;
+  oy1 = el1.offsetTop + el1.clientHeight / 2;
+
+  ox2 = el2.offsetLeft + el2.clientWidth / 2;
+  oy2 = el2.offsetTop + el2.clientHeight / 2;
+  
   el1.style.rotate =
     Math.PI * (object.pageX - ox1 < 0) +
     Math.atan((object.pageY - oy1) / (object.pageX - ox1)) +
